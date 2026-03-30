aeb5027113d823f475ee6e08f43426bcb29ac6f7
charmainema <charmaine.ma87@gmail.com>
2026-03-29 23:53:31 -0700

    added test for GearOptimizer

--- src/test/GearOptimizerTest.java
+++ src/test/GearOptimizerTest.java
@@ -1,6 +1,8 @@
 package test;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import main.model.GearOptimizer;
+import main.model.simulator.player.Player;
 import main.model.simulator.player.PlayerGear;
 
 public class GearOptimizerTest {
@@ -27,5 +30,12 @@ public class GearOptimizerTest {
         ArrayList<HashMap<String, PlayerGear>> gearSets = optimizer.getGearSets();
         assertEquals(1, gearSets.size());
         assertEquals(gear, gearSets.get(0));
+    }
+
+    @Test
+    void testGetters() {
+        // optimizer will create the player, so getter should not return null
+        Player player = optimizer.getMainPlayer();
+        assertNotNull(player);
     }
 }
